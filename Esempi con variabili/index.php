66846f9cd2695f91569140ff19eb9d73fdeea8cd
MarioGiagnotti <ing.giagnotti@gmail.com>
2025-05-03 16:12:03 +0200

    Aggiunti Dockerfile

---
+++ Esempi con variabili/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esempi variabili</title>
+</head>
+<body>
+    <h1>Esempi array</h1>
+    <a href="variabili1.php">Esempio variabili 1</a><br>
+    <a href="variabili2.php">Esempio variabili 2</a><br>
+</body>
+</html>
